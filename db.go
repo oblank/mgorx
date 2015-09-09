83de70be39565e82cc54995d6e1f67812941b681
oBlank <dyh1919@gmail.com>
2015-09-09 21:22:01 +0800

    update

--- db.go
+++ db.go
@@ -1,30 +1,30 @@
 package mgorx
 
 import (
-  "github.com/robfig/revel"
-  "labix.org/v2/mgo"
+	"github.com/revel/revel"
+	"gopkg.in/mgo.v2"
 )
 
 var (
-    mgoSession    *mgo.Session
-    database_name string
+	mgoSession    *mgo.Session
+	database_name string
 )
 
-func getSession () *mgo.Session {
-    if mgoSession == nil {
-        var err error
-        url, found := revel.Config.String("db.url")
-        if !found {
-             panic("db.url not set in config")
-        }
-        database_name, found = revel.Config.String("db.name")
-        if !found {
-             panic("db.name not set in config")
-        }
-        mgoSession, err = mgo.Dial(url)
-        if err != nil {
-             panic(err) // no, not really
-        }
-    }
-    return mgoSession.Clone()
+func getSession() *mgo.Session {
+	if mgoSession == nil {
+		var err error
+		url, found := revel.Config.String("db.url")
+		if !found {
+			panic("db.url not set in config")
+		}
+		database_name, found = revel.Config.String("db.name")
+		if !found {
+			panic("db.name not set in config")
+		}
+		mgoSession, err = mgo.Dial(url)
+		if err != nil {
+			panic(err) // no, not really
+		}
+	}
+	return mgoSession.Clone()
 }
